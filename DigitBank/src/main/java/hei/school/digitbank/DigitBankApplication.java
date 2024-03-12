ae4ae3e41475254c19d7120b60f4ae1f29b52d9d
Lovatiana15 <hei.lovatiana@gmail.com>
2024-03-12 10:15:44 +0300

    initial commit for app

---
+++ DigitBank/src/main/java/hei/school/digitbank/DigitBankApplication.java
@@ -0,0 +1,13 @@
+package hei.school.digitbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitBankApplication.class, args);
+	}
+
+}
---
+++ DigitBank/src/test/java/hei/school/digitbank/DigitBankApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.school.digitbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
